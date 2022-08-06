4cc4c864ad3d8f44a371bffa9fb7508268012569
Erdem Buzluk <buzlukerdem@gmail.com>
2022-08-06 16:39:05 +0300

    Tiers
    created entity layer, data access layer and business layer

---
+++ NKatmanliProje/EntityLayer/Concrete/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string City { get; set; }
+        public int Age { get; set; }     
+    }
+}
