ff896842d69b9ed753948da7fd4518a5495bd2e9
Erdem Buzluk <buzlukerdem@gmail.com>
2022-08-06 17:30:50 +0300

    abstract and interface
    created interface classes and repositories

---
+++ NKatmanliProje/DataAccessLayer/Abstract/IProductDal.cs
@@ -0,0 +1,17 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IProductDal
+    {
+        void Insert(Product p);
+        void Delete(Product p);
+        void Update(Product p);
+        List<Product> GetList();
+    }
+}
---
+++ NKatmanliProje/DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-HJL1ER0;database=DB_OOPSatis;integrated security=true");
+        }
+        public DbSet<Product>? Products { get; set; }
+        public DbSet<Customer>? Customers { get; set; }
+        public DbSet<Category>? Categories { get; set; }
+    }
+}
